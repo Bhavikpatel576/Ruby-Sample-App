24e63392a406e7f186bd4336675dceaeaec490e6
Bhavik Patel <Bhavikpatel576@gmail.com>
2017-02-21 20:53:29 -0800

    implment adv log in -3 errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def destroy 
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
@@ -12,6 +13,8 @@ class SessionsController < ApplicationController
   	if user && user.authenticate(params[:session][:password])
   		#Log the person in and redirect them to their webpage
   		log_in user
+      #Remember user checkbox 
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
   		#Rails automatically converts redirect_to user to the route user_url(user)
   		redirect_to user
   	else
